32978da5313f07262d8d2044d2d565da530af877
hacktiv8 <hacktiv8@hacktiv8-4.local>
2017-03-09 15:27:21 +0700

    Create react native init

--- reduxifyNews/src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-import NewsReducer from './reducer_news.js'
-import PeopleReducer from './reducer_people.js'
-const rootReducers = combineReducers({
-  news: NewsReducer,
-  peoples: PeopleReducer
-})
-
-export default rootReducers
